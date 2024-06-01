8041bb7f333b47b2d99eb4d31559b15ed8710d5b
Abel Adisu <abeladisu73@gmail.com>
2024-06-01 09:41:37 +0300

    Initial commit on paypal branch with spring boot integration

---
+++ src/main/java/com/act/paypal/integration/paypal/PaypalController.java
@@ -0,0 +1,78 @@
+package com.act.paypal.integration.paypal;
+
+import com.paypal.api.payments.Links;
+import com.paypal.api.payments.Payment;
+import com.paypal.base.rest.PayPalRESTException;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.view.RedirectView;
+
+@Controller
+@RequiredArgsConstructor
+@Slf4j
+public class PaypalController {
+
+    private final PaypalService paypalService;
+
+    @GetMapping("/")
+    public String home() {
+        return "index";
+    }
+
+    @PostMapping("/payment/create")
+    public RedirectView createPayment() {
+        try {
+            String cancelUrl = "http://localhost:8080/payment/cancel";
+            String successUrl = "http://localhost:8080/payment/success";
+            Payment payment = paypalService.createPayment(
+                    10.4,
+                    "USD",
+                    "paypal",
+                    "sale",
+                    "Payment description",
+                    successUrl,
+                    cancelUrl
+            );
+
+            for (Links links : payment.getLinks()) {
+                if (links.getRel().equals("approval_url")) {
+                    return new RedirectView(links.getHref());
+                }
+            }
+        } catch (PayPalRESTException ex) {
+            log.error("Error occurred during payment creation", ex);
+        }
+
+        return new RedirectView("/payment/error");
+    }
+
+    @GetMapping("/payment/success")
+    public String paymentSuccess(
+            @RequestParam("paymentId") String paymentId,
+            @RequestParam("PayerID") String payerId // Fixed parameter name
+    ) {
+        try {
+            Payment payment = paypalService.executePayment(paymentId, payerId);
+            if ("approved".equals(payment.getState())) {
+                return "paymentSuccess";
+            }
+        } catch (PayPalRESTException ex) {
+            log.error("Error occurred during payment execution", ex);
+        }
+        return "paymentError"; // Fixed return value for error
+    }
+
+    @GetMapping("/payment/cancel")
+    public String paymentCancel() {
+        return "paymentCancel";
+    }
+
+    @GetMapping("/payment/error")
+    public String paymentError() {
+        return "paymentError";
+    }
+}
